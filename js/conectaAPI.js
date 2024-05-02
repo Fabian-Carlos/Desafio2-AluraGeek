a70ebc245093c27c1f654ff67406abfe7d22a350
Fabian-Carlos <166092850+Fabian-Carlos@users.noreply.github.com>
2024-05-02 00:33:48 -0500

    Update conectaAPI.js

--- js/conectaAPI.js
+++ js/conectaAPI.js
@@ -1,5 +1,5 @@
 async function listaProductos(){
-    const conexion = await fetch("https://api-falsa.vercel.app/productos",{
+    const conexion = await fetch("https://api-fake-h15k.onrender.com/productos",{
         method:"GET",
         headers:{
         "Content-type":"application/json"
@@ -13,7 +13,7 @@ async function listaProductos(){
 }
 
 async function crearProducto(nombre,precio,imagen){
-    const conexion= await fetch("https://api-falsa.vercel.app/productos",{
+    const conexion= await fetch("https://api-fake-h15k.onrender.com/productos",{
     method:"POST",
     headers:{
         "Content-type":"application/json"
@@ -34,7 +34,7 @@ async function crearProducto(nombre,precio,imagen){
 
 async function eliminarProducto(id){
     console.log("estoy en datos, id:"+id);
-    const conexion = await fetch(`https://api-falsa.vercel.app/productos/${id}`, {
+    const conexion = await fetch(`https://api-fake-h15k.onrender.com/productos/${id}`, {
         method: "DELETE",
         headers: {
             "Content-type": "application/json"
