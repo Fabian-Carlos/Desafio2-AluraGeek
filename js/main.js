1746f8134dd9f8efb1ed18532658bb53c4489125
Fabian-Carlos <166092850+Fabian-Carlos@users.noreply.github.com>
2024-05-02 13:57:33 -0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -12,6 +12,7 @@ formulario.addEventListener("submit", (e) => {
     nombre: e.target.getElementById("nombre").value,
     precio: e.target.getElementById("precio").value,
     imagen: e.target.getElementById("imagen").value,
+    
   };
   localStorage.setItem("registro", JSON.stringify(listaRespuestas));
   console.log(listaRespuestas);
